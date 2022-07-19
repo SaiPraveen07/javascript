dbf0404d2fb56d0d7b0abc38b3cd4e429a7de555
SaiPraveen07 <107098201+SaiPraveen07@users.noreply.github.com>
2022-07-19 20:44:37 +0530

    JS. Math objects

---
+++ mathfunction.js
@@ -0,0 +1,144 @@
+// using Math.round()
+var num = Math.round(3.5);
+console.log(num);
+
+var num = Math.round(7.4);
+console.log(num);
+
+var num = Math.round(4.5);
+console.log(num);
+
+var num = Math.round(-4.5);
+console.log(num);
+
+var num = Math.round(null);
+console.log(num);
+
+var num = Math.round("JavaScript");
+console.log(num);
+
+// using math.ceil()
+let number = Math.ceil(6.4);
+console.log(number);
+
+let int1 = Math.ceil(47.8);
+console.log(int1);
+
+let int2 = Math.ceil(22.4);
+console.log(int2);
+
+let value = Math.ceil("JavaScript");
+console.log(value);
+
+//using math.floor()
+let a = 57.8;
+
+let roundedNumber = Math.floor(a);
+console.log(roundedNumber);
+
+let s = Math.floor(2.4);
+console.log(s);
+
+let i = Math.floor(-0.456);
+console.log(i);
+
+//using math.trunc()
+let n = Math.trunc("4.56");
+console.log(n);
+
+let n1 = Math.trunc(-10.456);
+console.log(n1);
+
+//using math.sign()
+let k = Math.sign(3);
+console.log(k);
+
+let k1 = Math.sign(-7);
+console.log(k1);
+
+//using math.pow()
+let power = Math.pow(3, 2);
+console.log(power);
+
+let power1 = Math.pow(-5, -2);
+console.log(power1);
+
+//using math.sqrt()
+let j = Math.sqrt(42);
+console.log(j);
+
+let j1 = Math.sqrt(167.44);
+console.log(j1);
+
+//using math.abs()
+val1 = Math.abs(57);
+console.log(val1);
+
+val2 = Math.abs(-3);
+console.log(val2);
+
+//using math.sin()
+let value1 = Math.sin(7);
+console.log(value1);
+
+let value2 = Math.sin((90 * Math.PI) / 180);
+console.log(value2);
+let value3 = Math.sin(Math.PI);
+console.log(value3);
+
+//using math.cos()
+let c1 = Math.cos((0 * Math.PI) / 90);
+console.log(c1);
+
+let c2 = Math.cos(-2);
+console.log(c2);
+
+//using math.max()
+let M1 = Math.max(86, 74, 11, 7, 54);
+console.log(M1);
+
+let M2 = Math.max(-1, -71, -180);
+console.log(M2);
+
+//Using math.min()
+let m1 = Math.min(-2, -41, -132);
+console.log(m1);
+
+let m2 = Math.min(0.431, 76, 680);
+console.log(m2);
+
+//using math.random()
+let l;
+l = Math.random();
+console.log(l);
+
+l = Math.floor(Math.random() * 42 + 7);
+console.log(l);
+
+//using math.log()
+let l1 = Math.log(1);
+console.log(l1);
+
+let l2 = Math.log(Math.E);
+console.log(l2);
+
+//using math.log2()
+let v1 = Math.log2(7);
+console.log(v1);
+
+let v2 = Math.log2(4);
+console.log(v2);
+
+//using math.log10()
+let s1 = Math.log10(100);
+console.log(s1);
+
+var s2 = Math.log10(1000);
+console.log(s2);
+
+//using math.exp()
+let z1 = Math.exp(5);
+console.log(z1);
+
+let z2 = Math.exp(10);
+console.log(z2);
